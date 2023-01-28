fea891c535b09062ea1dbc24590455cd96256309
leon <liqiyoungman@163.com>
2023-01-28 16:46:01 +0800

    1

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -1,17 +1,7 @@
-import common from '../../utils/common';
+import common from "../../utils/common";
 export default {
-  // localBaseUrl: 'http://zkcx.cxstar.cn',
-  // publishBaseUrl: 'http://zkcx.cxstar.cn',
-  // localBaseUrl: 'https://www.knowledgesource.cn',
-  // publishBaseUrl: 'https://www.knowledgesource.cn',
   localBaseUrl: common.baseUrl,
   publishBaseUrl: common.baseUrl,
 
-  // publishBaseUrl: 'http://admin.globalbooks.test',
-  // publishBaseUrl: 'http://192.168.0.236:5555',
-  // publishBaseUrl: 'http://www.cxstar.test',
-
-  fileBaseUrl: common.fileUrl,
-  // fileBaseUrl: 'https://file.cxstar.cn',
-  // fileBaseUrl: 'http://file.cxstar.cn:16710',
-}
+  fileBaseUrl: common.fileUrl
+};
